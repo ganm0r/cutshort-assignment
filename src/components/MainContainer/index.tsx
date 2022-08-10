e39691087b58c5ec0e39a7f40f19c2d5fa99afc0
ganm0r <gndhrvmv@gmail.com>
2022-08-10 19:04:02 +0530

    :recycle: Turn main container into flexbox

--- src/components/MainContainer/index.tsx
+++ src/components/MainContainer/index.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
+  display: flex;
   width: 100vw;
   height: 100vh;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default MainContainer;
