8b53aba0e9d1ee2e4d4e774b3c1a26c5f92f955b
ganm0r <gndhrvmv@gmail.com>
2022-08-10 19:02:10 +0530

    :lipstick: Create global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { edenTheme } from 'theme';
 
+import colors from 'theme/constants/colors';
+import typography from 'theme/constants/typography';
 import App from './App';
 
 const root = document.getElementById('root');
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: ${colors.white};
+    overflow: hidden;
+    font-family: ${typography.fonts.primary};
+    color: ${colors.black}; 
+  }
+`;
+
 ReactDOM.render(
   <ThemeProvider theme={edenTheme}>
+    <GlobalStyle />
     <App />
   </ThemeProvider>,
   root
