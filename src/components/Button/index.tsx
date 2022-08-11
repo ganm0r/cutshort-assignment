07ad5a8d6e0f8bd43c65cfecae0092cfa866984d
ganm0r <gndhrvmv@gmail.com>
2022-08-11 17:49:16 +0530

    :construction: Build Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import colors from 'theme/constants/colors';
+// import typography from 'theme/constants/typography';
 
-function Button() {
+const StyledButton = styled.button`
+  background-color: ${colors.purple};
+  font-size: 18px;
+  box-sizing: border-box;
+  color: ${colors.white};
+  padding: 16px;
+  outline: none;
+  border-radius: 6px;
+  cursor: pointer;
+  margin-top: 2%;
+  border: none;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    transfrom: scale(0.98);
+  }
+`;
+
+interface ButtonProps {
+  children: React.ReactNode;
+  type: 'button' | 'submit' | 'reset' | undefined;
+}
+
+function Button({ children, type }: ButtonProps): JSX.Element {
   return (
-    <div>Button</div>
-  )
+    <StyledButton type={type}>
+      {children}
+    </StyledButton>
+  );
 }
 
 export default Button;
