420ac5d2f577142e9b393570b6aca9f62c7119db
ganm0r <gndhrvmv@gmail.com>
2022-08-12 19:02:26 +0530

    :construction: Build Setup page

--- src/pages/Setup/index.tsx
+++ src/pages/Setup/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import typography from 'theme/constants/typography';
 import colors from 'theme/constants/colors';
+
+import Input from 'components/Input';
+import Button from 'components/Button';
+import CustomInput from 'components/CustomInput';
 
 const Heading = styled.h1`
   margin: 12px;
@@ -28,21 +32,72 @@ const Flex = styled.div`
   gap: 8px;
 `;
 
-function Setup() {
+const Form = styled.form`
+  display: grid;
+  position: relative;
+  grid-template-rows: repeat(3, 0.5fr);
+`;
+
+interface SetupProps {
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function Setup({ setActiveStep }: SetupProps): JSX.Element {
+  const [formData, setFormData] = useState({
+    wname: '',
+    wurl: '',
+  });
+
+  const { wname, wurl } = formData;
+
+  const onChange = (event) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setActiveStep(1);
+  };
   return (
     <Grid
       style={{
-        gridTemplateRows: '2fr repeat(1fr, 3)',
+        gridTemplateRows: '1fr 2fr',
       }}
     >
       <Flex>
-        <Heading>
-          Let&apos;s set up a home for all your work
-        </Heading>
-        <SubHeading>
-          You can always create another workspace later.
-        </SubHeading>
+        <Heading>Let&apos;s set up a home for all your work</Heading>
+        <SubHeading>You can always create another workspace later.</SubHeading>
       </Flex>
+      <Form onSubmit={onSubmit}>
+        <Input
+          title="Workspace Name"
+          optional={false}
+          type="text"
+          placeholder="Eden"
+          required
+          id="wname"
+          name="wname"
+          value={wname}
+          onChange={onChange}
+        />
+        <CustomInput
+          title="Workspace URL"
+          url="www.eden.com/"
+          optional
+          type="text"
+          placeholder="Example"
+          required={false}
+          id="wurl"
+          name="wurl"
+          value={wurl}
+          onChange={onChange}
+        />
+        <Button type="submit">Create Workspace</Button>
+      </Form>
     </Grid>
   );
 }
