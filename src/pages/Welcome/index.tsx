f6336c624e7429cabc493c42054a0beaf0c2bc28
ganm0r <gndhrvmv@gmail.com>
2022-08-12 19:01:58 +0530

    :construction: Build Welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import typography from 'theme/constants/typography';
 import colors from 'theme/constants/colors';
+
+import Input from 'components/Input';
+import Button from 'components/Button';
 
 const Heading = styled.h1`
   margin: 12px;
@@ -28,21 +31,72 @@ const Flex = styled.div`
   gap: 8px;
 `;
 
-function Welcome() {
+const Form = styled.form`
+  display: grid;
+  position: relative;
+  grid-template-rows: repeat(3, 0.5fr);
+`;
+
+interface WelcomeProps {
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function Welcome({ setActiveStep }: WelcomeProps): JSX.Element {
+  const [formData, setFormData] = useState({
+    fname: '',
+    dname: '',
+  });
+
+  const { fname, dname } = formData;
+
+  const onChange = (event) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    setActiveStep(1);
+  };
+
   return (
     <Grid
       style={{
-        gridTemplateRows: '2fr repeat(1fr, 3)',
+        gridTemplateRows: '1fr 2fr',
       }}
     >
       <Flex>
-        <Heading>
-          Welcome! First things first...
-        </Heading>
-        <SubHeading>
-          You can always change them later.
-        </SubHeading>
+        <Heading>Welcome! First things first...</Heading>
+        <SubHeading>You can always change them later.</SubHeading>
       </Flex>
+      <Form onSubmit={onSubmit}>
+        <Input
+          title="Full Name"
+          type="text"
+          optional={false}
+          placeholder="Steve Jobs"
+          required
+          id="fname"
+          name="fname"
+          value={fname}
+          onChange={onChange}
+        />
+        <Input
+          title="Display Name"
+          optional={false}
+          type="text"
+          placeholder="Steve"
+          required
+          id="dname"
+          name="dname"
+          value={dname}
+          onChange={onChange}
+        />
+        <Button type="submit">Create Workspace</Button>
+      </Form>
     </Grid>
   );
 }
